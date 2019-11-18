52ec01c8a46b66453adf41afb73520da206265c7
halilseker <g141210082@sakarya.edu.tr>
2019-11-18 11:06:27 +0300

    Added searching to employee profile

--- employee_meetings_api/views.py
+++ employee_meetings_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from employee_meetings_api import serializers
 from  employee_meetings_api import models
@@ -14,3 +15,5 @@ class EmployeeProfileViewSet(viewsets.ModelViewSet):
     queryset = models.EmployeeProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
